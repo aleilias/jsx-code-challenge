cc9640dec7e09090641019619c3cee174f523728
aleilias <86530708+aleilias@users.noreply.github.com>
2021-12-23 15:26:08 -0600

    challenge

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 //Create a react app from scratch.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Things I like</h1>
+    <ul>
+      <li>Eating</li>
+      <li>Reading</li>
+      <li>Programming</li>
+    </ul>
+  </div>,
+  document.querySelector("#root")
+);
+
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
